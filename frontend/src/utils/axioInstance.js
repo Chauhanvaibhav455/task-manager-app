0e331e4ba0b157e79d6a18ecf3098162434660cb
vaibhav <157279616+Chauhanvaibhav455@users.noreply.github.com>
2026-05-01 15:25:53 +0530

    fix calendar path case

--- frontend/src/utils/axioInstance.js
+++ frontend/src/utils/axioInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:5000/api" 
+const BASE_URL = "https://task-manager-app-production-86de.up.railway.app/api"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
